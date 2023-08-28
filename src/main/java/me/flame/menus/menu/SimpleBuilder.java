1ebf89aedcb5e7035f92ea97c2aa68608e3a9021
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-08-28 19:30:37 +0300

    Allow the MenuType to be applied

--- src/main/java/me/flame/menus/menu/SimpleBuilder.java
+++ src/main/java/me/flame/menus/menu/SimpleBuilder.java
@@ -5,9 +5,12 @@ public class SimpleBuilder extends BaseBuilder<Menu, SimpleBuilder> {
         super();
     }
 
+    @Override
     public Menu create() {
         checkRows(rows);
-        final Menu menu = new Menu(rows, title, modifiers);
+        final Menu menu = type == MenuType.CHEST
+                ? new Menu(rows, title, modifiers)
+                : new Menu(type, title, modifiers);
         if (menuConsumer != null) menuConsumer.accept(menu);
         return menu;
     }
