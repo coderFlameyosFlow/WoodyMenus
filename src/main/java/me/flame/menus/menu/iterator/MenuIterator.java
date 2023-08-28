564c85006ee7ad59d471ba274b6011d86d08f7dd
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-08-28 18:55:04 +0300

    Implement Iterator<MenuItem>
    Also turn 0 indexed algorithm into 1 indexed algorithm

--- src/main/java/me/flame/menus/menu/iterator/MenuIterator.java
+++ src/main/java/me/flame/menus/menu/iterator/MenuIterator.java
@@ -1,9 +1,12 @@
 package me.flame.menus.menu.iterator;
 
+import me.flame.menus.items.MenuItem;
 import me.flame.menus.menu.BaseMenu;
 import me.flame.menus.menu.Slot;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
 
 /**
  *
@@ -12,197 +15,205 @@ import org.jetbrains.annotations.Nullable;
  *
  * A special class made to iterate over complex menus.
  */
-public final class MenuIterator {
+public final class MenuIterator implements Iterator<MenuItem> {
 
-	@NotNull
-	private Slot currentPosition;
+    @NotNull
+    private Slot currentPosition;
 
-	@NotNull
-	private final IterationDirection direction;
+    @NotNull
+    private final IterationDirection direction;
 
-	@NotNull
-	private final BaseMenu<?> menu;
+    @NotNull
+    private final BaseMenu<?> menu;
 
-	public MenuIterator(int startingRow, int startingCol,
-											@NotNull IterationDirection direction,
-											@NotNull BaseMenu<?> menu) {
-		this.menu = menu;
-		this.currentPosition = new Slot(startingRow, startingCol);
-		this.direction = direction;
-	}
+    public MenuIterator(int startingRow, int startingCol,
+                        @NotNull IterationDirection direction,
+                        @NotNull BaseMenu<?> menu) {
+        this.menu = menu;
+        this.currentPosition = new Slot(startingRow, startingCol);
+        this.direction = direction;
+    }
 
-	public @Nullable Slot nextSlot(boolean emptyOnly) {
+    public @Nullable Slot nextSlot(boolean emptyOnly) {
+        if (!emptyOnly) {
+            Slot newPos = direction.shift(currentPosition, menu.getRows());
+            if (newPos.getRow() >= menu.getRows() || newPos.getRow() < 1
+                    || newPos.getColumn() > 9 || newPos.getColumn() < 1) {
+                return null;
+            }
+            currentPosition = newPos;
+            return newPos;
+        }
 
-		if(!emptyOnly) {
-			Slot newPos = direction.shift(currentPosition, menu.getRows());
-			if(newPos.getRow() >= menu.getRows() || newPos.getRow() < 0
-							|| newPos.getColumn() > 8 || newPos.getColumn() < 0) {
-				return null;
-			}
-			currentPosition = newPos;
-			return newPos;
-		}
+        while (menu.hasItem(currentPosition)) {
+            Slot shiftedPos = direction.shift(currentPosition, menu.getRows());
 
-		while(menu.getOptionalItem(currentPosition).isPresent()) {
-			Slot shiftedPos = direction.shift(currentPosition, menu.getRows());
+            if (shiftedPos.getRow() >= menu.getRows() || shiftedPos.getRow() < 1
+                    || shiftedPos.getColumn() > 9 || shiftedPos.getColumn() < 1) {
+                return null;
+            }
 
-			if(shiftedPos.getRow() >= menu.getRows() || shiftedPos.getRow() < 0
-							|| shiftedPos.getColumn() > 8 || shiftedPos.getColumn() < 0) {
-				return null;
-			}
+            currentPosition = shiftedPos;
+        }
 
-			currentPosition = shiftedPos;
-		}
+        //when it becomes empty
+        return currentPosition;
+    }
 
-		//when it becomes empty
-		return currentPosition;
-	}
+    public @Nullable Slot nextSlot() {
+        return nextSlot(false);
+    }
 
-	public @Nullable Slot nextSlot() {
-		return nextSlot(false);
-	}
+    @Override
+    public boolean hasNext() {
+        return currentPosition.getRow() >= menu.getRows() || currentPosition.getRow() < 1
+                || currentPosition.getColumn() > 9 || currentPosition.getColumn() < 1;
+    }
 
-	public enum IterationDirection {
+    @Override
+    public MenuItem next() {
+        return menu.getItem(nextSlot());
+    }
 
-
-		HORIZONTAL() {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-
-				int oldCol = oldPos.getColumn();
-				int oldRow = oldPos.getRow();
-
-				if(oldCol == 8 && oldRow < maxRows-1) {
-					oldRow++;
-					oldCol = 0;
-				}else {
-					oldCol++;
-				}
-
-				return new Slot(oldRow, oldCol);
-			}
-		},
-
-		VERTICAL() {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				int oldCol = oldPos.getColumn();
-				int oldRow = oldPos.getRow();
-
-				if(oldCol < 8 && oldRow == maxRows-1) {
-					oldCol++;
-					oldRow = 0;
-				}else {
-					oldRow++;
-				}
-
-				return new Slot(oldRow, oldCol);
-			}
-		},
-
-		UPWARDS_ONLY {
-
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				int row = oldPos.getRow()-1;
-				if(row < 0) {
-					row = oldPos.getRow();
-				}
-				return new Slot(row, oldPos.getColumn());
-			}
-		},
-
-		DOWNWARDS_ONLY {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				int row = oldPos.getRow()+1;
-				if(row > maxRows) {
-					row = oldPos.getRow();
-				}
-				return new Slot(row, oldPos.getColumn());
-			}
-
-		},
-
-		RIGHT_ONLY {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				int col = oldPos.getColumn()+1;
-				if(col > 8) {
-					col = oldPos.getColumn();
-				}
-				return new Slot(oldPos.getRow(), col);
-			}
-		},
+    public enum IterationDirection {
 
 
-		LEFT_ONLY{
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				int col = oldPos.getColumn()-1;
-				if(col < 0) {
-					col = oldPos.getColumn();
-				}
-				return new Slot(oldPos.getRow(), col);
-			}
-		},
+        HORIZONTAL() {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+
+                int oldCol = oldPos.getColumn();
+                int oldRow = oldPos.getRow();
+
+                if (oldCol == 9 && oldRow < maxRows) {
+                    oldRow++;
+                    oldCol = 1;
+                } else {
+                    oldCol++;
+                }
+
+                return new Slot(oldRow, oldCol);
+            }
+        },
+
+        VERTICAL() {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                int oldCol = oldPos.getColumn();
+                int oldRow = oldPos.getRow();
+
+                if (oldCol < 9 && oldRow == maxRows) {
+                    oldCol++;
+                    oldRow = 0;
+                } else {
+                    oldRow++;
+                }
+
+                return new Slot(oldRow, oldCol);
+            }
+        },
+
+        UPWARDS_ONLY {
+
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                int row = oldPos.getRow();
+                if (row < 0) {
+                    row = oldPos.getRow();
+                }
+                return new Slot(row, oldPos.getColumn());
+            }
+        },
+
+        DOWNWARDS_ONLY {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                int row = oldPos.getRow()+1;
+                if(row > maxRows) {
+                    row = oldPos.getRow();
+                }
+                return new Slot(row, oldPos.getColumn());
+            }
+
+        },
+
+        RIGHT_ONLY {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                int col = oldPos.getColumn()+1;
+                if(col > 8) {
+                    col = oldPos.getColumn();
+                }
+                return new Slot(oldPos.getRow(), col);
+            }
+        },
 
 
-		RIGHT_UPWARDS_ONLY {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				Slot upwardSlot = UPWARDS_ONLY.shift(oldPos, maxRows);
-				int row = upwardSlot.getRow();
-
-				Slot rightSlot = RIGHT_ONLY.shift(oldPos, maxRows);
-				int col = rightSlot.getColumn();
-
-				return new Slot(row, col);
-			}
-		},
+        LEFT_ONLY{
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                int col = oldPos.getColumn()-1;
+                if(col < 0) {
+                    col = oldPos.getColumn();
+                }
+                return new Slot(oldPos.getRow(), col);
+            }
+        },
 
 
-		RIGHT_DOWNWARDS_ONLY {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				Slot downwardSlot = DOWNWARDS_ONLY.shift(oldPos, maxRows);
-				int row = downwardSlot.getRow();
+        RIGHT_UPWARDS_ONLY {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                Slot upwardSlot = UPWARDS_ONLY.shift(oldPos, maxRows);
+                int row = upwardSlot.getRow();
 
-				Slot rightSlot = RIGHT_ONLY.shift(oldPos, maxRows);
-				int col = rightSlot.getColumn();
+                Slot rightSlot = RIGHT_ONLY.shift(oldPos, maxRows);
+                int col = rightSlot.getColumn();
 
-				return new Slot(row, col);
-			}
-		},
-
-		LEFT_UPWARDS {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				Slot upwardSlot = UPWARDS_ONLY.shift(oldPos, maxRows);
-				int row = upwardSlot.getRow();
-
-				Slot leftSlot = LEFT_ONLY.shift(oldPos, maxRows);
-				int col = leftSlot.getColumn();
-
-				return new Slot(row, col);
-			}
-		},
-
-		LEFT_DOWNWARDS {
-			@Override
-			Slot shift(Slot oldPos, int maxRows) {
-				Slot downwardSlot = DOWNWARDS_ONLY.shift(oldPos, maxRows);
-				int row = downwardSlot.getRow();
-
-				Slot leftSlot = LEFT_ONLY.shift(oldPos, maxRows);
-				int col = leftSlot.getColumn();
-
-				return new Slot(row, col);
-			}
-		};
-
-		abstract Slot shift(Slot oldPos, int maxRows);
-	}
+                return new Slot(row, col);
+            }
+        },
 
 
+        RIGHT_DOWNWARDS_ONLY {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                Slot downwardSlot = DOWNWARDS_ONLY.shift(oldPos, maxRows);
+                int row = downwardSlot.getRow();
+
+                Slot rightSlot = RIGHT_ONLY.shift(oldPos, maxRows);
+                int col = rightSlot.getColumn();
+
+                return new Slot(row, col);
+            }
+        },
+
+        LEFT_UPWARDS {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                Slot upwardSlot = UPWARDS_ONLY.shift(oldPos, maxRows);
+                int row = upwardSlot.getRow();
+
+                Slot leftSlot = LEFT_ONLY.shift(oldPos, maxRows);
+                int col = leftSlot.getColumn();
+
+                return new Slot(row, col);
+            }
+        },
+
+        LEFT_DOWNWARDS {
+            @Override
+            Slot shift(Slot oldPos, int maxRows) {
+                Slot downwardSlot = DOWNWARDS_ONLY.shift(oldPos, maxRows);
+                int row = downwardSlot.getRow();
+
+                Slot leftSlot = LEFT_ONLY.shift(oldPos, maxRows);
+                int col = leftSlot.getColumn();
+
+                return new Slot(row, col);
+            }
+        };
+
+        abstract Slot shift(Slot oldPos, int maxRows);
+    }
 }
