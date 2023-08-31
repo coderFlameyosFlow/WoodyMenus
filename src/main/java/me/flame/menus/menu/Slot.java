c6e279cc302325809bf7dca46b4d6e028cebccc9
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-08-31 11:17:47 +0300

    Fix Algorithm
    (col + (row - 1) * 9) - 1
    This would reproduce "0" when row is 1 and col is 1, but should be 8.
    
    Using this algorithm: ((col + (row - 1)) * 9) - 1
    now math knows what to multiply, so "8" is returned as it should.

--- src/main/java/me/flame/menus/menu/Slot.java
+++ src/main/java/me/flame/menus/menu/Slot.java
@@ -2,23 +2,23 @@ package me.flame.menus.menu;
 
 import lombok.Getter;
 
-import javax.annotation.concurrent.NotThreadSafe;
-import javax.annotation.concurrent.ThreadSafe;
-
-@ThreadSafe
 public final class Slot {
-    private static final int MAX_INVENTORY_SLOTS = 54;
     private final @Getter int row;
     private final int col;
-    private final @Getter int slot;
+    private @Getter int slot;
 
     public Slot(int row, int col) {
         this.row = row;
         this.col = col;
-        this.slot = Math.min(((col + (row - 1) * 9) - 1), MAX_INVENTORY_SLOTS);
+        this.slot = ((col + (row - 1)) * 9) - 1;;
+        slot = slot >= 54 ? -1 : slot;
     }
 
     public int getColumn() {
         return col;
+    }
+
+    public boolean isSlot() {
+        return slot >= 0;
     }
 }
