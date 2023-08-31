836ced9e98403f188b15ee9482667316546ba2fc
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-08-31 11:43:38 +0300

    MenuException extends RE now

--- src/main/java/me/flame/menus/exceptions/MenuException.java
+++ src/main/java/me/flame/menus/exceptions/MenuException.java
@@ -1,6 +1,6 @@
 package me.flame.menus.exceptions;
 
-public final class MenuException extends Exception {
+public final class MenuException extends RuntimeException {
     public MenuException(String message) {
         super(message);
     }
