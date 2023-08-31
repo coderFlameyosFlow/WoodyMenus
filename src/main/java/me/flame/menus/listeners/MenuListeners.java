f85f45f6892db957d8bd394e7795470bf5b517f6
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-08-31 11:35:23 +0300

    Increase readability and fix bugs.
    Bugs fixed:
    when executing inventory click actions, only outside click actions are executed, as in when an action isn't null the outside click action is executed. fun right? that was fixed

--- src/main/java/me/flame/menus/listeners/MenuListeners.java
+++ src/main/java/me/flame/menus/listeners/MenuListeners.java
@@ -30,8 +30,8 @@ import lombok.val;
 import me.flame.menus.components.nbt.ItemNbt;
 import me.flame.menus.items.MenuItem;
 import me.flame.menus.menu.BaseMenu;
-
 import me.flame.menus.menu.PaginatedMenu;
+
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -45,93 +45,82 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public final class MenuListeners implements Listener {
-    private static final Set<InventoryAction> TAKE = EnumSet.of(InventoryAction.PICKUP_ONE, InventoryAction.PICKUP_SOME, InventoryAction.PICKUP_HALF, InventoryAction.PICKUP_ALL, InventoryAction.COLLECT_TO_CURSOR, InventoryAction.HOTBAR_SWAP, InventoryAction.MOVE_TO_OTHER_INVENTORY);
-    private static final Set<InventoryAction> PLACE = EnumSet.of(InventoryAction.PLACE_ONE, InventoryAction.PLACE_SOME, InventoryAction.PLACE_ALL);
-    private static final Set<InventoryAction> SWAP = EnumSet.of(InventoryAction.HOTBAR_SWAP, InventoryAction.SWAP_WITH_CURSOR, InventoryAction.HOTBAR_MOVE_AND_READD);
-    private static final Set<InventoryAction> DROP = EnumSet.of(InventoryAction.DROP_ONE_SLOT, InventoryAction.DROP_ALL_SLOT, InventoryAction.DROP_ONE_CURSOR, InventoryAction.DROP_ALL_CURSOR);
+    private static final EnumSet<InventoryAction> TAKE = EnumSet.of(
+            InventoryAction.PICKUP_ONE,
+            InventoryAction.PICKUP_SOME,
+            InventoryAction.PICKUP_HALF,
+            InventoryAction.PICKUP_ALL,
+            InventoryAction.COLLECT_TO_CURSOR,
+            InventoryAction.HOTBAR_SWAP,
+            InventoryAction.MOVE_TO_OTHER_INVENTORY
+    );
+
+    private static final EnumSet<InventoryAction> PLACE = EnumSet.of(
+            InventoryAction.PLACE_ONE,
+            InventoryAction.PLACE_SOME,
+            InventoryAction.PLACE_ALL
+    );
+
+    private static final EnumSet<InventoryAction> SWAP = EnumSet.of(
+            InventoryAction.HOTBAR_SWAP,
+            InventoryAction.SWAP_WITH_CURSOR,
+            InventoryAction.HOTBAR_MOVE_AND_READD
+    );
+
+    private static final EnumSet<InventoryAction> DROP = EnumSet.of(
+            InventoryAction.DROP_ONE_SLOT,
+            InventoryAction.DROP_ALL_SLOT,
+            InventoryAction.DROP_ONE_CURSOR,
+            InventoryAction.DROP_ALL_CURSOR
+    );
 
     private static final Event.Result denied = Event.Result.DENY;
     private static final InventoryType player = InventoryType.PLAYER;
     private static final InventoryAction otherInv = InventoryAction.MOVE_TO_OTHER_INVENTORY;
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onInventoryClick(InventoryClickEvent event) {
-        InventoryHolder holder = event.getInventory().getHolder();
-        if (!(holder instanceof BaseMenu<?>)) return;
-        BaseMenu<?> menu = (BaseMenu<?>) holder;
-
-        Inventory ci = event.getClickedInventory();
+    public void onInventoryClick(final @NotNull InventoryClickEvent event) {
         Inventory inv = event.getInventory();
+        InventoryHolder holder = inv.getHolder();
+        if (!(holder instanceof BaseMenu<?>)) return;
+
+        BaseMenu<?> m = (BaseMenu<?>) holder;
+        Inventory ci = event.getClickedInventory();
         InventoryAction action = event.getAction();
 
         if (ci == null) {
-            val outsideClick = menu.getOutsideClickAction();
-            if (outsideClick == null) return;
-            outsideClick.accept(event);
+            val outsideClick = m.getOutsideClickAction();
+            if (outsideClick != null) outsideClick.accept(event);
             return;
         }
+        Objects.requireNonNull(ci);
 
-        InventoryType ciType = ci.getType();
-        InventoryType invType = inv.getType();
-
-        boolean unremovable = !menu.areItemsRemovable();
-        if ((!menu.areItemsPlaceable() && isPlaceItemEvent(ci, action, ciType, invType)) ||
-                (unremovable && isTakeItemEvent(ci, action, ciType, invType)) ||
-                (!menu.areItemsSwappable() && isSwapItemEvent(ci, action, ciType, invType)) ||
-                (unremovable && isDropItemEvent(ci, action, invType)) ||
-                (!menu.areItemsCloneable() && isOtherEvent(ci, action, invType)))
-            event.setResult(denied);
-
-        val topClick = menu.getOutsideClickAction();
-        val bottomClick = menu.getOutsideClickAction();
-        val defaultClick = menu.getOutsideClickAction();
-
-        if (topClick != null && inv.equals(ci)) topClick.accept(event);
-        else if (bottomClick != null && event.getView().getBottomInventory().equals(ci)) bottomClick.accept(event);
-        else if (defaultClick != null) defaultClick.accept(event);
-
-        MenuItem guiItem;
-
-        // Checks whether it's a paginated menu or not
-        if (menu instanceof PaginatedMenu) {
-            final PaginatedMenu paginatedGui = (PaginatedMenu) menu;
-
-            // Gets the menu item from the added items or the page items
-            guiItem = paginatedGui.getItem(event.getSlot());
-            if (guiItem == null) guiItem = paginatedGui.getFromPageItems(event.getSlot());
-
-        } else {
-            // The clicked GUI Item
-            guiItem = menu.getItem(event.getSlot());
-        }
-
-        if (!isMenuItem(event.getCurrentItem(), guiItem)) return;
-
-        // Executes the action of the item
-        final Consumer<InventoryClickEvent> itemAction = guiItem.getClickAction();
-        if (itemAction != null) itemAction.accept(event);
+        denyIfModifierApplied(event, m, ci, action, ci.getType(), inv.getType());
+        executeActions(event, m, inv, ci);
+        executeMenuItem(event, event.getCurrentItem(), m, event.getSlot());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onGuiDrag(final InventoryDragEvent event) {
+    public void onGuiDrag(final @NotNull InventoryDragEvent event) {
         val inventory = event.getInventory();
         val holder = inventory.getHolder();
         if (!(holder instanceof BaseMenu<?>)) return;
         val menu = (BaseMenu<?>) holder;
-        val rawSlots = event.getRawSlots();
 
+        val rawSlots = event.getRawSlots();
         if (!menu.areItemsPlaceable() || isDraggingOnGui(inventory, rawSlots))
             event.setResult(denied);
-        final Consumer<InventoryDragEvent> dragAction = menu.getDragAction();
+        val dragAction = menu.getDragAction();
         if (dragAction != null) dragAction.accept(event);
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onGuiClose(final InventoryCloseEvent event) {
+    public void onGuiClose(final @NotNull InventoryCloseEvent event) {
         val holder = event.getInventory().getHolder();
         if (!(holder instanceof BaseMenu<?>)) return;
         val menu = (BaseMenu<?>) holder;
@@ -141,7 +130,7 @@ public final class MenuListeners implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onGuiOpen(final InventoryOpenEvent event) {
+    public void onGuiOpen(final @NotNull InventoryOpenEvent event) {
         val holder = event.getInventory().getHolder();
         if (!(holder instanceof BaseMenu<?>)) return;
         val menu = (BaseMenu<?>) holder;
@@ -186,17 +175,46 @@ public final class MenuListeners implements Listener {
         return action == InventoryAction.CLONE_STACK || action == InventoryAction.UNKNOWN;
     }
 
-    /**
-     * Checks if the item is or not a GUI item
-     *
-     * @param currentItem The current item clicked
-     * @param guiItem     The GUI item in the slot
-     * @return Whether it is or not a GUI item
-     */
-    private boolean isMenuItem(@Nullable final ItemStack currentItem, @Nullable final MenuItem guiItem) {
-        if (currentItem == null || guiItem == null) return false;
-        final String nbt = ItemNbt.getString(currentItem, "woody-menu");
-        if (nbt == null) return false;
-        return nbt.equals(guiItem.getUniqueId().toString());
+    private void executeActions(final @NotNull InventoryClickEvent event,
+                                   final @NotNull BaseMenu<?> m,
+                                   final @NotNull Inventory inv,
+                                   final Inventory ci) {
+        val topClick = m.getTopClickAction();
+        val bottomClick = m.getBottomClickAction();
+        val defaultClick = m.getClickAction();
+
+        if (topClick != null && inv.equals(ci)) topClick.accept(event);
+        else if (bottomClick != null && event.getView().getBottomInventory().equals(ci)) bottomClick.accept(event);
+        else if (defaultClick != null) defaultClick.accept(event);
+    }
+
+    private void denyIfModifierApplied(final @NotNull InventoryClickEvent event,
+                                       final @NotNull BaseMenu<?> m,
+                                       final @NotNull Inventory ci,
+                                       final @NotNull InventoryAction action,
+                                       final @NotNull InventoryType ciType,
+                                       final @NotNull InventoryType invType) {
+        boolean unremovable = !m.areItemsRemovable();
+        if ((!m.areItemsPlaceable() && isPlaceItemEvent(ci, action, ciType, invType)) ||
+                (unremovable && isTakeItemEvent(ci, action, ciType, invType)) ||
+                (!m.areItemsSwappable() && isSwapItemEvent(ci, action, ciType, invType)) ||
+                (unremovable && isDropItemEvent(ci, action, invType)) ||
+                (!m.areItemsCloneable() && isOtherEvent(ci, action, invType)))
+            event.setResult(denied);
+    }
+
+    private void executeMenuItem(final @NotNull InventoryClickEvent event,
+                                 final @Nullable ItemStack it,
+                                 final @NotNull BaseMenu<?> m,
+                                 final int num) {
+        MenuItem menuItem = (m instanceof PaginatedMenu)
+                ? (m.getOptionalItem(num).orElse(((PaginatedMenu) m).getFromPageItems(num)))
+                : (m.getItem(num));
+        if (it == null || menuItem == null) return;
+        final String nbt = ItemNbt.getString(it, "woody-menu");
+        if (nbt == null || !nbt.equals(menuItem.getUniqueId().toString())) return;
+
+        final Consumer<InventoryClickEvent> itemAction = menuItem.getClickAction();
+        if (itemAction != null) itemAction.accept(event);
     }
 }
