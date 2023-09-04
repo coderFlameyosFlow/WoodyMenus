ca11673e65480a88711bef28f0783ceaa0789228
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-05 00:31:04 +0300

    Allow Pdc to get the Plugin

--- src/main/java/me/flame/menus/components/nbt/ItemNbt.java
+++ src/main/java/me/flame/menus/components/nbt/ItemNbt.java
@@ -1,41 +1,19 @@
-/**
- * MIT License
- * <p>
- * Copyright (c) 2021 TriumphTeam
- * Copyright (c) 2023 FlameyosFlow
- * <p>
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * <p>
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * <p>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package me.flame.menus.components.nbt;
 
 import me.flame.menus.util.VersionHelper;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Ideally this wouldn't need to be a util, but because of the {@link LegacyNbt} it makes it easier. Legacy
  * Will use the PDC wrapper if version is higher than 1.14
+ * @author TriumphTeam
  */
-public final class ItemNbt {
 
-    private static final NbtWrapper nbt = VersionHelper.IS_PDC_VERSION ? new Pdc() : new LegacyNbt();
+//changed
+public final class ItemNbt {
+    public static final NbtWrapper WRAPPER = VersionHelper.IS_PDC_VERSION ? new Pdc(JavaPlugin.getProvidingPlugin(ItemNbt.class)) : new LegacyNbt();
 
     /**
      * Sets an NBT tag to the an {@link ItemStack}.
@@ -46,7 +24,7 @@ public final class ItemNbt {
      * @return An {@link ItemStack} that has NBT set.
      */
     public static ItemStack setString(@NotNull final ItemStack itemStack, @NotNull final String key, @NotNull final String value) {
-        return nbt.setString(itemStack, key, value);
+        return WRAPPER.setString(itemStack, key, value);
     }
 
     /**
@@ -57,7 +35,7 @@ public final class ItemNbt {
      * @return The tag that was stored in the {@link ItemStack}.
      */
     public static String getString(@NotNull final ItemStack itemStack, @NotNull final String key) {
-        return nbt.getString(itemStack, key);
+        return WRAPPER.getString(itemStack, key);
     }
 
     /**
@@ -70,7 +48,7 @@ public final class ItemNbt {
      * @return An {@link ItemStack} with a boolean value set.
      */
     public static ItemStack setBoolean(@NotNull final ItemStack itemStack, @NotNull final String key, final boolean value) {
-        return nbt.setBoolean(itemStack, key, value);
+        return WRAPPER.setBoolean(itemStack, key, value);
     }
 
     /**
@@ -81,6 +59,6 @@ public final class ItemNbt {
      * @return An {@link ItemStack} that has the tag removed.
      */
     public static ItemStack removeTag(@NotNull final ItemStack itemStack, @NotNull final String key) {
-        return nbt.removeTag(itemStack, key);
+        return WRAPPER.removeTag(itemStack, key);
     }
 }
