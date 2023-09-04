19438eadc428222424c957bcdf91a7861ab1f566
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-04 23:34:33 +0300

    Improvements
    Use "super.clone()" on .clone().
    Improve performance by internalizing click event which is executed at MenuListeners
    "of" now exists; don't use MenuItem constructor

--- src/main/java/me/flame/menus/items/MenuItem.java
+++ src/main/java/me/flame/menus/items/MenuItem.java
@@ -1,13 +1,12 @@
 package me.flame.menus.items;
 
-import lombok.EqualsAndHashCode;
-
 import me.flame.menus.components.nbt.ItemNbt;
 
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -35,20 +34,27 @@ import java.util.function.Consumer;
  *      // the lambda (Consumer) at ItemBuilder#buildItem(Consumer) is optional and you do not have to provide an action, you can use ItemBuilder#buildItem()
  * }</pre>
  */
-
-//changed
-
-@EqualsAndHashCode
 @SuppressWarnings("unused")
 public final class MenuItem implements Cloneable {
-    private @Nullable Consumer<InventoryClickEvent> clickAction;
-    private ItemStack itemStack;
-    private final UUID uuid = UUID.randomUUID();
+    private Consumer<InventoryClickEvent> clickAction;
+    ItemStack itemStack;
 
+    private UUID uuid;
+
+    @ApiStatus.Obsolete
     public MenuItem(ItemStack itemStack, @Nullable Consumer<InventoryClickEvent> action) {
         Objects.requireNonNull(itemStack);
+        this.uuid =  UUID.randomUUID();
         this.itemStack = ItemNbt.setString(itemStack, "woody-menu", uuid.toString());
-        this.clickAction = action;
+        this.clickAction = action == null ? (event -> {}) : action;
+    }
+
+    public static @NotNull MenuItem of(ItemStack itemStack, @Nullable Consumer<InventoryClickEvent> action) {
+        return new MenuItem(itemStack, action);
+    }
+
+    public static @NotNull MenuItem of(ItemStack itemStack) {
+        return new MenuItem(itemStack, null);
     }
 
     public @Nullable Consumer<InventoryClickEvent> getClickAction() {
@@ -76,13 +82,31 @@ public final class MenuItem implements Cloneable {
         this.itemStack = ItemNbt.setString(itemStack, "woody-menu", uuid.toString());
     }
 
-    public Material getType() {
+    public @NotNull Material getType() {
         return itemStack.getType();
     }
 
-    @SuppressWarnings("MethodDoesntCallSuperMethod")
     @Override
-    public MenuItem clone() {
-        return new MenuItem(itemStack, clickAction);
+    public boolean equals(Object o) {
+        if (!(o instanceof MenuItem)) return false;
+        MenuItem item = (MenuItem) o;
+
+        if (this == item) return true;
+        return uuid.equals(item.uuid) && itemStack.equals(item.itemStack);
+    }
+
+    @Override
+    public @NotNull MenuItem clone() {
+        try {
+            MenuItem item = (MenuItem) super.clone();
+            item.uuid = uuid;
+            return item;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void click(InventoryClickEvent event) {
+        clickAction.accept(event);
     }
 }
