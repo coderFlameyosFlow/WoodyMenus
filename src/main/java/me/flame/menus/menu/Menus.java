5f3c638a4b9b71ea9af19ef245325f9dcfa80742
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-05 00:09:21 +0300

    Fix bugs, deprecate MenuFactory

--- src/main/java/me/flame/menus/menu/Menus.java
+++ src/main/java/me/flame/menus/menu/Menus.java
@@ -4,18 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import me.flame.menus.listeners.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public final class Menus {
-    public static final Plugin PLUGIN = JavaPlugin.getProvidingPlugin(Menus.class);
-
-    static {
-        Bukkit.getPluginManager().registerEvents(new MenuListeners(), PLUGIN);
-    }
-
     private static final MenuFactory menuFactory = new MenuFactory();
+
+    public static void init(Plugin plugin) {
+        Bukkit.getPluginManager().registerEvents(new MenuListeners(plugin), plugin);
+    }
 
     @Contract(value = " -> new", pure = true)
     public static @NotNull SimpleBuilder menu() {
@@ -32,7 +31,11 @@ public final class Menus {
      * <p>
      * This is a much less verbose way to create menus, and much less "Java-eey"
      * @return a less verbose way of creating menus; The MenuFactory
+     * @since 1.0.0
+     * @deprecated Use menu constructors like {@link Menu} or use builders like {@link Menus#menu()}
      */
+    @Deprecated
+    @ApiStatus.ScheduledForRemoval(inVersion = "1.4.0")
     public static MenuFactory getFactory() {
         return menuFactory;
     }
