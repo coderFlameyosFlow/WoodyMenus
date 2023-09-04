2b755bebd2aff606ee7790ac56f60d215bee1863
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-05 00:18:33 +0300

    Public Constructor

--- src/main/java/me/flame/menus/menu/Menu.java
+++ src/main/java/me/flame/menus/menu/Menu.java
@@ -6,19 +6,27 @@ import java.util.EnumSet;
 import java.util.Set;
 
 public final class Menu extends BaseMenu<Menu> {
-    Menu(int rows, String title, EnumSet<Modifier> modifiers, boolean colorize) {
+    public Menu(int rows, String title, EnumSet<Modifier> modifiers, boolean colorize) {
         super(rows, title, modifiers, colorize);
     }
 
-    Menu(int rows, String title, EnumSet<Modifier> modifiers) {
+    public Menu(int rows, String title, EnumSet<Modifier> modifiers) {
         super(rows, title, modifiers);
     }
 
-    Menu(MenuType type, String title, EnumSet<Modifier> modifiers) {
+    public Menu(MenuType type, String title, EnumSet<Modifier> modifiers) {
         super(type, title, modifiers);
     }
 
-    Menu(MenuType type, String title, EnumSet<Modifier> modifiers, boolean colorize) {
+    public Menu(int rows, String title) {
+        super(rows, title, EnumSet.noneOf(Modifier.class));
+    }
+
+    public Menu(MenuType type, String title) {
+        super(type, title, EnumSet.noneOf(Modifier.class));
+    }
+
+    public Menu(MenuType type, String title, EnumSet<Modifier> modifiers, boolean colorize) {
         super(type, title, modifiers, colorize);
     }
 }
