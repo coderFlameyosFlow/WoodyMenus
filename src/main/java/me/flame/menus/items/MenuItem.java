be6f23d62fadc2e135d8b4b819d11e72df55f61d
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-14 04:08:01 +0300

    Additions.
    make the action package private
    MenuItem now implements ConfigurationSerializable; be able to add MenuItem to configs more easily
    add hashCode(), it just uses the uuid's hashcode since that's fastest and most convenient.

--- src/main/java/me/flame/menus/items/MenuItem.java
+++ src/main/java/me/flame/menus/items/MenuItem.java
@@ -3,14 +3,18 @@ package me.flame.menus.items;
 import me.flame.menus.components.nbt.ItemNbt;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -18,28 +22,32 @@ import java.util.function.Consumer;
 /**
  * A Gui itemStack which was particularly made to have an action.
  * <p>
- * It can be used for MCMAPI (Woody is the recommended menus library for MCMAPI, you can use whatever you like).
+ * It can be used for MCMAPI.
  * <p>
- * Good example of using "GuiItem":
+ * Good example of using "MenuItem":
  * <pre>{@code
- *      var guiItem = ...;
- *      guiItem.setClickAction(event -> {
+ *      var menuItem = ...;
+ *      menuItem.setClickAction(event -> {
  *          ...
  *      });
  *
  *      // implementing a new itemStack:
- *      menu.addItem(new ItemBuilder(itemStack, 2) // 2 is the amount of items you get from this "ItemBuilder"
+ *      menu.addItem(ItemBuilder.of(itemStack, 2) // 2 is the amount of items you get from this "ItemBuilder"
  *                                  .setName(...).setLore(...)
  *                                  .buildItem(() -> ...);
  *      // the lambda (Consumer) at ItemBuilder#buildItem(Consumer) is optional and you do not have to provide an action, you can use ItemBuilder#buildItem()
  * }</pre>
  */
 @SuppressWarnings("unused")
-public final class MenuItem implements Cloneable {
-    private Consumer<InventoryClickEvent> clickAction;
+public final class MenuItem implements Cloneable, ConfigurationSerializable {
+    @NotNull
+    Consumer<InventoryClickEvent> clickAction;
+
+    @NotNull
     ItemStack itemStack;
 
-    private UUID uuid;
+    @NotNull
+    private final UUID uuid;
 
     @ApiStatus.Obsolete
     public MenuItem(ItemStack itemStack, @Nullable Consumer<InventoryClickEvent> action) {
@@ -57,11 +65,11 @@ public final class MenuItem implements Cloneable {
         return new MenuItem(itemStack, null);
     }
 
-    public @Nullable Consumer<InventoryClickEvent> getClickAction() {
+    public @NotNull Consumer<InventoryClickEvent> getClickAction() {
         return clickAction;
     }
 
-    public void setClickAction(@Nullable Consumer<InventoryClickEvent> clickAction) {
+    public void setClickAction(@NotNull Consumer<InventoryClickEvent> clickAction) {
         this.clickAction = clickAction;
     }
 
@@ -70,7 +78,7 @@ public final class MenuItem implements Cloneable {
         return new ItemEditor(this);
     }
 
-    public ItemStack getItemStack() {
+    public @NotNull ItemStack getItemStack() {
         return itemStack;
     }
 
@@ -98,9 +106,7 @@ public final class MenuItem implements Cloneable {
     @Override
     public @NotNull MenuItem clone() {
         try {
-            MenuItem item = (MenuItem) super.clone();
-            item.uuid = uuid;
-            return item;
+            return (MenuItem) super.clone();
         } catch (CloneNotSupportedException e) {
             throw new RuntimeException(e);
         }
@@ -108,5 +114,38 @@ public final class MenuItem implements Cloneable {
 
     public void click(InventoryClickEvent event) {
         clickAction.accept(event);
+    }
+
+    @Override
+    public @NotNull Map<String, Object> serialize() {
+        Map<String, Object> result = new LinkedHashMap<>();
+
+        result.put("type", getType().name());
+        result.put("uuid", uuid);
+
+        ItemMeta meta = itemStack.getItemMeta();
+        int amount = itemStack.getAmount();
+
+        if (amount != 1) result.put("amount", amount);
+        if (meta != null) result.put("meta", meta);
+
+        return result;
+    }
+
+    public @NotNull MenuItem deserialize(@NotNull Map<String, Object> serialized) {
+        String type = (String) serialized.get("type");
+        int amount = (int) serialized.getOrDefault("amount", 1);
+        ItemMeta meta = (ItemMeta) serialized.get("meta");
+        UUID uuid = (UUID) serialized.get("uuid");
+
+        ItemStack result = new ItemStack(Material.valueOf(type), amount);
+        if (meta != null) result.setItemMeta(meta);
+
+        return MenuItem.of(itemStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode(); // they provide a fast hashcode
     }
 }
