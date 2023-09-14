35e4d9610c62ba00382dc589100c8030e294248a
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-14 05:49:51 +0300

    Rework next() hasNext() & constructors
    Improved performance by the first and next position being cached (thoroughly tested)
    Use Slot.FIRST.copy() as a way to save some performance (copying is very fast thanks to the fast copy private constructor)
    Add documentation

--- src/main/java/me/flame/menus/menu/iterator/MenuIterator.java
+++ src/main/java/me/flame/menus/menu/iterator/MenuIterator.java
@@ -2,7 +2,9 @@ package me.flame.menus.menu.iterator;
 
 import me.flame.menus.items.MenuItem;
 import me.flame.menus.menu.BaseMenu;
+import me.flame.menus.menu.IterationDirection;
 import me.flame.menus.menu.Slot;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,239 +21,140 @@ import java.util.Optional;
  */
 @SuppressWarnings("unused")
 public final class MenuIterator implements Iterator<MenuItem> {
-
     @NotNull
-    private Slot currentPosition;
+    private Slot position;
 
     @NotNull
     private final IterationDirection direction;
 
+
     @NotNull
     private final BaseMenu<?> menu;
 
-    private final int menuRows;
+    private Slot next;
+
+    private boolean isFirst = true;
+
+    private final int rows;
+
+    private static final String NOTHING_MORE_NEXT =
+            "Used PageIterator#next() but nothing more" +
+            "\nFix: Use hasNext() beforehand to avoid this error.";
+
+    private static final String NOTHING_MORE_NEXT_OPTIONAL =
+            "Used PageIterator#nextOptional() but nothing more" +
+            "\nFix: Use hasNext() beforehand to avoid this error.";
+
+    private static final String NOTHING_MORE_NEXT_NOT_NULL =
+            "Used PageIterator#nextNotNull() but nothing more";
+
+    private static final String GREATER_THAN_ONE_ONLY =
+            "Starting row and column must be 1 or greater, col 9 and under or rows 6 and under only." +
+            "\nFix: If you're using an algorithm for rows/cols, you might wanna check it";
 
     public MenuIterator(int startingRow, int startingCol,
                         @NotNull IterationDirection direction,
                         @NotNull BaseMenu<?> menu) {
-        if (startingRow < 1 || startingCol < 1)
-            throw new IllegalArgumentException(
-                    "Starting row and column must be greater than 1 or equal. not under 0" +
-                    "\nFix: If you're using an algorithm for rows and/or cols, you might wanna check it."
-            );
+        Slot prepos = new Slot(startingRow, startingCol);
+        if (!prepos.isSlot()) throw new IllegalArgumentException(GREATER_THAN_ONE_ONLY);
         this.menu = menu;
-        this.currentPosition = new Slot(startingRow, startingCol);
+        this.position = prepos;
         this.direction = direction;
-        this.menuRows = menu.getRows();
+
+        this.rows = menu.getRows();
+    }
+    public MenuIterator(@NotNull IterationDirection direction,
+                        @NotNull BaseMenu<?> menu) {
+        this.menu = menu;
+        this.position = Slot.FIRST.copy();
+        this.direction = direction;
+
+        this.rows = menu.getRows();
     }
 
-    public @Nullable Slot nextSlot(boolean emptyOnly) {
-        if (!emptyOnly) {
-            Slot newPos = direction.shift(currentPosition, this.menuRows);
-            currentPosition = newPos;
-            if (!currentPosition.isSlot()) {
-                return null;
-            }
-            return newPos;
-        }
+    /**
+     * Retrieves the next slot in the menu.
+     *
+     * @param emptyOnly  a boolean indicating whether to retrieve only empty slots
+     * @return           the next empty slot in the menu, or null if no empty slot is found
+     */
+    public Slot nextSlot(boolean emptyOnly) {
+        if (!emptyOnly) return nextSlot();
 
-        while (menu.hasItem(currentPosition)) {
-            Slot shiftedPos = direction.shift(currentPosition, this.menuRows);
+        while (true) {
+            MenuItem item = menu.getItem(position);
+            if (item == null) break;
 
-            if (!shiftedPos.isSlot()) {
-                return null;
-            }
-
-            currentPosition = shiftedPos;
+            position = direction.shift(position, this.rows);
+            if (!position.isSlot()) return null;
         }
 
         // when it becomes empty
-        return currentPosition;
+        return position;
     }
 
+    /**
+     * Retrieves the next slot in the menu.
+     * @return           the next empty slot in the menu, or null if no empty slot is found
+     */
     public @Nullable Slot nextSlot() {
-        Slot newPos = direction.shift(currentPosition, this.menuRows);
-        currentPosition = newPos;
-        return !newPos.isSlot() ? null : newPos;
+        if (!isFirst) {
+            position = next;
+            next = null;
+            return position;
+        }
+        isFirst = false;
+        return position;
     }
 
     @Override
     public boolean hasNext() {
-        return currentPosition.isSlot();
+        if (next != null) return true;
+        next = direction.shift(position, this.rows);
+        return next.isSlot();
     }
 
     @Override
-    public @Nullable MenuItem next() {
+    public MenuItem next() {
         Slot slot = nextSlot();
-        if (slot == null)
-            throw new NoSuchElementException("Used MenuIterator#next() but no more items to iterate over");
-        return menu.getItem(slot);
+        if (slot != null) return menu.getItem(slot.getSlot());
+        throw new NoSuchElementException(NOTHING_MORE_NEXT);
     }
 
-    public @NotNull MenuItem nextNotNull() {
-        // don't be fooled, this is only 1 loop through the menu, not 1 AND recursion, only one happens.
-        Slot slot = nextSlot();
-        if (slot == null) {
-            while (hasNext()) {
-                slot = nextSlot();
-                if (slot == null) continue;
-                MenuItem item = menu.getItem(slot);
-                if (item != null) {
-                    return item;
-                }
-            }
+    /**
+     * Retrieves the next non-null MenuItem in the menu.
+     *
+     * @return         	the next non-null MenuItem in the menu
+     */
+    public MenuItem nextNotNull() {
+        while (hasNext()) {
+            Slot slot = nextSlot();
+            if (slot == null)
+                throw new NoSuchElementException(NOTHING_MORE_NEXT_NOT_NULL +
+                    "\nFix: Everything after slot " + position.getSlot() + " is empty/null."
+                );
 
-            throw new NoSuchElementException("Used MenuIterator#next() but no more items to iterate over");
+            MenuItem item = menu.getItem(slot.getSlot());
+            if (item != null) return item;
         }
 
-        MenuItem item = menu.getItem(slot);
-        if (item != null) {
-            return item;
-        }
-        return nextNotNull();
+        throw new NoSuchElementException(NOTHING_MORE_NEXT_NOT_NULL +
+            "\nFix: Everything after slot " + position.getSlot() + " is empty/null."
+        );
     }
 
+
+    /**
+     * Retrieves the next optional menu item.
+     *
+     * @return         	an Optional object containing the next MenuItem, if available.
+     *                  Returns an empty Optional if the slot in the menu is empty.
+     *                  Returns an empty Optional if there are no more items.
+     * @throws NoSuchElementException if there are no more items in the menu.
+     */
     public Optional<MenuItem> nextOptional() {
         Slot slot = nextSlot();
-        if (slot == null)
-            throw new NoSuchElementException("Used MenuIterator#next() but no more items to iterate over");
-        return menu.get(slot);
-    }
-
-    public enum IterationDirection {
-        HORIZONTAL() {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-
-                int oldCol = oldPos.getColumn();
-                int oldRow = oldPos.getRow();
-
-                if (oldCol == 9 && oldRow < maxRows) {
-                    oldCol = 1;
-                    oldRow++;
-                } else {
-                    oldCol++;
-                }
-
-                return new Slot(oldRow, oldCol);
-            }
-        },
-
-        VERTICAL() {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                int oldCol = oldPos.getColumn();
-                int oldRow = oldPos.getRow();
-
-                if (oldCol < 9 && oldRow == maxRows) {
-                    oldCol++;
-                    oldRow = 1;
-                } else {
-                    oldRow++;
-                }
-
-                return new Slot(oldRow, oldCol);
-            }
-        },
-
-        UPWARDS_ONLY {
-
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                int oldRow = oldPos.getRow();
-                int row = oldRow - 1;
-                if (row < 1) {
-                    row = oldRow;
-                }
-                return new Slot(row, oldPos.getColumn());
-            }
-        },
-
-        DOWNWARDS_ONLY {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                int oldRow = oldPos.getRow();
-                int row = oldRow + 1;
-                if (row > maxRows) {
-                    row = oldRow;
-                }
-                return new Slot(row, oldPos.getColumn());
-            }
-        },
-
-        RIGHT_ONLY {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                int oldCol = oldPos.getColumn() + 1;
-                int col = oldCol > 9 ? oldCol - 1 : oldCol;
-                return new Slot(oldPos.getRow(), col);
-            }
-        },
-
-
-        LEFT_ONLY {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                int oldCol = oldPos.getColumn() - 1;
-                int col = oldCol < 1 ? oldCol + 1 : oldCol;
-                return new Slot(oldPos.getRow(), col);
-            }
-        },
-
-
-        RIGHT_UPWARDS_ONLY {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                Slot upwardSlot = UPWARDS_ONLY.shift(oldPos, maxRows);
-                int row = upwardSlot.getRow();
-
-                Slot rightSlot = RIGHT_ONLY.shift(oldPos, maxRows);
-                int col = rightSlot.getColumn();
-
-                return new Slot(row, col);
-            }
-        },
-
-
-        RIGHT_DOWNWARDS_ONLY {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                Slot downwardSlot = DOWNWARDS_ONLY.shift(oldPos, maxRows);
-                int row = downwardSlot.getRow();
-
-                Slot rightSlot = RIGHT_ONLY.shift(oldPos, maxRows);
-                int col = rightSlot.getColumn();
-
-                return new Slot(row, col);
-            }
-        },
-
-        LEFT_UPWARDS {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                Slot upwardSlot = UPWARDS_ONLY.shift(oldPos, maxRows);
-                int row = upwardSlot.getRow();
-
-                Slot leftSlot = LEFT_ONLY.shift(oldPos, maxRows);
-                int col = leftSlot.getColumn();
-
-                return new Slot(row, col);
-            }
-        },
-
-        LEFT_DOWNWARDS {
-            @Override
-            Slot shift(Slot oldPos, int maxRows) {
-                Slot downwardSlot = DOWNWARDS_ONLY.shift(oldPos, maxRows);
-                int row = downwardSlot.getRow();
-
-                Slot leftSlot = LEFT_ONLY.shift(oldPos, maxRows);
-                int col = leftSlot.getColumn();
-
-                return new Slot(row, col);
-            }
-        };
-
-        abstract Slot shift(Slot oldPos, int maxRows);
+        if (slot != null) return menu.get(slot);
+        throw new NoSuchElementException(NOTHING_MORE_NEXT_OPTIONAL);
     }
 }
