9b3a0b2ae2d5c48a1b2ccce2f61219d0b9ca344d
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-14 05:15:10 +0300

    Additions & simplified algorithm
    Added setters
    Added custom constants like FIRST which is 0 in slot and NaS which stands for Not A Slot.
    Added copy() and custom constructor for faster copy 
    Make slot, row and col package private so every class in the same package can benefit from not using getters.

--- src/main/java/me/flame/menus/menu/Slot.java
+++ src/main/java/me/flame/menus/menu/Slot.java
@@ -1,24 +1,122 @@
 package me.flame.menus.menu;
 
-import lombok.Getter;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("unused")
 public final class Slot {
-    private final @Getter int row;
-    private final int col;
-    private @Getter int slot;
+    int row, slot, col;
 
-    public Slot(int row, int col) {
+    public static final Slot FIRST = new Slot(1, 1);
+    public static final Slot NaS = new Slot(8, 1);
+
+    public Slot(final int row, final int col) {
         this.row = row;
         this.col = col;
-        this.slot = ((row - 1) * 9) + (col - 1);
-        slot = slot >= 54 ? -1 : slot;
+        this.slot = ((row * 9) + col) - 10;
+        this.slot = slot >= 54 ? -1 : slot;
     }
 
+    public Slot(final int slot) {
+        this.row = (slot / 9) + 1;
+        this.col = (slot % 9) + 1;
+        this.slot = slot >= 54 ? -1 : slot;
+    }
+
+    // fast copy constructor
+    private Slot(final int row, final int col, final boolean dummy) {
+        this.row = row;
+        this.col = col;
+    }
+
+    /**
+     * Sets the slot at the specified row and column.
+     *
+     * @param row the row index of the slot
+     * @param col the column index of the slot
+     * @return the updated Slot object
+     */
+    @Contract("_, _ -> this")
+    public Slot setSlot(final int row, final int col) {
+        if (row == this.row && col == this.col) return this;
+
+        this.row = row;
+        this.col = col;
+        this.slot = ((row * 9) + col) - 10;
+        this.slot = slot >= 54 ? -1 : slot;
+        return this;
+    }
+
+    /**
+     * Sets the slot at the specified row and column.
+     * @param slot the slot
+     * @return the updated Slot object
+     */
+    @Contract("_ -> this")
+    @SuppressWarnings("UnusedReturnValue")
+    public Slot setSlot(@NotNull final Slot slot) {
+        this.row = slot.row;
+        this.col = slot.col;
+        this.slot = slot.slot;
+        return this;
+    }
+
+    /**
+     * Sets the slot at the specified row and column.
+     * @param slot the slot
+     * @return the updated Slot object
+     */
+    @Contract("_ -> this")
+    @SuppressWarnings("UnusedReturnValue")
+    public Slot setSlot(final int slot) {
+        this.row = (slot / 9) + 1;
+        this.col = (slot % 9) + 1;
+        this.slot = slot >= 54 ? -1 : slot;
+        return this;
+    }
+
+    /**
+     * Creates a copy of the Slot object.
+     *
+     * @return	A new Slot object that is an exact copy of the original Slot.
+     */
+    @NotNull
+    public Slot copy() {
+        final Slot slot = new Slot(row, col, true);
+        slot.slot = this.slot;
+        return slot;
+    }
+
+    /**
+     * Get the column of the slot.
+     * @return the column
+     */
     public int getColumn() {
         return col;
     }
 
+    /**
+     * Get the row of the slot.
+     * @return the row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * Get the slot.
+     * @return the slot
+     */
+    public int getSlot() {
+        return slot;
+    }
+
+    /**
+     * Check if the slot is a valid slot.
+     * @apiNote If the slot is equal to -1, it's invalid; fast check.
+     * @return true if the slot does not equal -1.
+     */
     public boolean isSlot() {
-        return slot >= 0;
+        return slot != -1;
     }
 }
