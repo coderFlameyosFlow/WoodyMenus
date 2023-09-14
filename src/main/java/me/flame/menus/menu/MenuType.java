1f03ccd2724b55cf8941eb65e2d8f113b6cb82b9
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-14 05:42:19 +0300

    Make cleaner

--- src/main/java/me/flame/menus/menu/MenuType.java
+++ src/main/java/me/flame/menus/menu/MenuType.java
@@ -6,6 +6,7 @@ import org.bukkit.event.inventory.InventoryType;
 
 import lombok.Getter;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public enum MenuType {
     CHEST(InventoryType.CHEST, 9),
@@ -15,6 +16,6 @@ public enum MenuType {
     DISPENSER(InventoryType.DISPENSER, 8),
     BREWING(InventoryType.BREWING, 4);
 
-    private final @Getter InventoryType type;
-    private final @Getter int limit;
+    private final InventoryType type;
+    private final int limit;
 }
