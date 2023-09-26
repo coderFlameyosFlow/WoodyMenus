a1476007e01f6d4236d26cfb137d241e5b0cd145
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-26 22:17:56 +0300

    New Filling API

--- src/main/java/me/flame/menus/menu/fillers/Filler.java
+++ src/main/java/me/flame/menus/menu/fillers/Filler.java
@@ -1,94 +1,73 @@
 package me.flame.menus.menu.fillers;
 
-import lombok.val;
-import lombok.var;
 import me.flame.menus.items.MenuItem;
 import me.flame.menus.menu.BaseMenu;
+
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("unused")
 public final class Filler implements MenuFiller {
     @NotNull
-    private final BaseMenu<?> menu;
+    private final BaseMenu menu;
 
-    private Filler(@NotNull BaseMenu<?> menu) {
+    private Filler(@NotNull BaseMenu menu) {
         this.menu = menu;
     }
 
     @NotNull
     @Contract(value = "_ -> new", pure = true)
-    public static Filler from(@NotNull BaseMenu<?> menu) {
+    public static Filler from(@NotNull BaseMenu menu) {
         return new Filler(menu);
     }
 
-    /**
-     * Fills the menu with the specified border material.
-     *
-     * @param  borderMaterial the material to be used for filling the borders
-     */
     public void fill(Material borderMaterial) {
         final int size = menu.getSize();
 
+        MenuItem stack = MenuItem.of(new ItemStack(borderMaterial));
         for (int i = 0; i < size; i++) {
-            val item = menu.getItem(i);
+            MenuItem item = menu.getItem(i);
             if (item == null || item.getType() == Material.AIR) {
-                menu.setItem(i, new ItemStack(borderMaterial));
+                menu.setItem(i, stack);
             }
         }
     }
 
-    /**
-     * Fills the menu with the specified border material.
-     * @param menuItem the material to be used for filling the borders
-     */
     public void fill(@NotNull MenuItem menuItem) {
         final int size = menu.getSize();
 
         for (int i = 0; i < size; i++) {
-            val item = menu.getItem(i);
+            MenuItem item = menu.getItem(i);
             if (item == null || item.getType() == Material.AIR) {
                 menu.setItem(i, menuItem);
             }
         }
     }
 
-    /**
-     * Fills the with the specified border material.
-     *
-     * @param  itemStack the material to be used for filling the borders
-     */
-    public void fill(ItemStack itemStack) {
+    public void fill(ItemStack itemS) {
         final int size = menu.getSize();
 
+        MenuItem stack = MenuItem.of(itemS);
         for (int i = 0; i < size; i++) {
-            val item = menu.getItem(i);
+            MenuItem item = menu.getItem(i);
             if (item == null || item.getType() == Material.AIR) {
-                menu.setItem(i, itemStack);
+                menu.setItem(i, stack);
             }
         }
     }
 
-    /**
-     * Fills the borders of the menu with the specified border material.
-     *
-     * @param  borderMaterial the material to be used for filling the borders
-     */
     public void fillBorders(Material borderMaterial) {
         final int size = menu.getSize();
-        final var itemStack = new ItemStack(borderMaterial);
 
+        final MenuItem stack = MenuItem.of(new ItemStack(borderMaterial));
         for (int i = 0; i < size; i++) {
-            if (MenuFiller.isBorderSlot(i, size)) menu.setItem(i, itemStack);
+            if (MenuFiller.isBorderSlot(i, size)) menu.setItem(i, stack);
         }
     }
 
-    /**
-     * Fills the borders of the menu with the specified border material.
-     *
-     * @param  borderMaterial the material to be used for filling the borders
-     */
     public void fillBorders(@NotNull MenuItem borderMaterial) {
         final int size = menu.getSize();
 
@@ -97,16 +76,66 @@ public final class Filler implements MenuFiller {
         }
     }
 
-    /**
-     * Fills the borders of the menu with the specified border material.
-     *
-     * @param  itemStack the material to be used for filling the borders
-     */
     public void fillBorders(ItemStack itemStack) {
         final int size = menu.getSize();
 
+        final MenuItem stack = MenuItem.of(itemStack);
         for (int i = 0; i < size; i++) {
-            if (MenuFiller.isBorderSlot(i, size)) menu.setItem(i, itemStack);
+            if (MenuFiller.isBorderSlot(i, size)) menu.setItem(i, stack);
         }
     }
+
+    public void fillRow(int row, Material borderMaterial) {
+        final int size = menu.getSize();
+
+        MenuItem itemStack = MenuItem.of(new ItemStack(borderMaterial));
+        for (int i = 0; i < size; i++) {
+            if (i / 9 == row && !menu.hasItem(i)) menu.setItem(i, itemStack);
+        }
+    }
+
+    public void fillRow(int row, ItemStack borderMaterial) {
+        final int size = menu.getSize();
+
+        MenuItem itemStack = MenuItem.of(borderMaterial);
+        for (int i = 0; i < size; i++) {
+            if (i / 9 == row && !menu.hasItem(i)) menu.setItem(i, itemStack);
+        }
+    }
+
+    public void fillRow(int row, MenuItem itemStack) {
+        final int size = menu.getSize();
+
+        for (int i = 0; i < size; i++) {
+            if ((i / 9) == row && !menu.hasItem(i)) menu.setItem(i, itemStack);
+        }
+    }
+
+    public void fillArea(int length, int width, Material borderMaterial) {
+        final int size = menu.getSize();
+
+        MenuItem itemStack = MenuItem.of(new ItemStack(borderMaterial));
+        for (int i = 0; i < size; i++) {
+            if (MenuFiller.isInArea(i, length, width)) menu.setItem(i, itemStack);
+        }
+    }
+
+    public void fillArea(int length, int width, ItemStack borderMaterial) {
+        final int size = menu.getSize();
+
+        MenuItem itemStack = MenuItem.of(borderMaterial);
+        for (int i = 0; i < size; i++) {
+            if (MenuFiller.isInArea(i, length, width)) menu.setItem(i, itemStack);
+        }
+    }
+
+    public void fillArea(int length, int width, MenuItem itemStack) {
+        final int size = menu.getSize();
+
+        for (int i = 0; i < size; i++) {
+            if (MenuFiller.isInArea(i, length, width)) menu.setItem(i, itemStack);
+        }
+    }
+
+    // isInArea = simple geometry to check if a slot is in an area of L*W
 }
