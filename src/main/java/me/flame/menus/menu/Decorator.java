ebdbb294de314e8e97a0aab2eb57fbcbe32a53a2
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2023-09-26 22:31:50 +0300

    New Decorating API

--- src/main/java/me/flame/menus/menu/Decorator.java
+++ src/main/java/me/flame/menus/menu/Decorator.java
@@ -47,4 +47,58 @@ public interface Decorator {
      * @param  itemStack  the item stack to fill the pages with
      */
     void fillPages(ItemStack itemStack);
+
+    /**
+     * Fill all the <strong>EMPTY</strong> slots in a specific row.
+     * @param row the row to fill
+     * @param borderMaterial the material to fill the row with
+     */
+    void fillRow(int row, Material borderMaterial);
+
+    /**
+     * Fill all the <strong>EMPTY</strong> slots in a specific row.
+     * @param row the row to fill
+     * @param borderMaterial the material to fill the row with
+     */
+    void fillRow(int row, ItemStack borderMaterial);
+
+    /**
+     * Fill all the <strong>EMPTY</strong> slots in a specific row.
+     * @param row the row to fill
+     * @param itemStack material to fill the row with
+     */
+    void fillRow(int row, MenuItem itemStack);
+
+    /**
+     * Fill all the empty slots in the A of Length * Width;
+     * check out the implementation of "isInArea" before using this method
+     * @param length the length.
+     *               check out some geometry if you don't know what "length" is supposed to mean
+     * @param width the width
+     *              check out some geometry if you don't know what "length" is supposed to mean
+     * @param borderMaterial the material
+     */
+    void fillArea(int length, int width, Material borderMaterial);
+
+    /**
+     * Fill all the empty slots in the A of Length * Width;
+     * check out the implementation of "isInArea" before using this method
+     * @param length the length.
+     *               check out some geometry if you don't know what "length" is supposed to mean
+     * @param width the width
+     *              check out some geometry if you don't know what "length" is supposed to mean
+     * @param borderMaterial the material
+     */
+    void fillArea(int length, int width, ItemStack borderMaterial);
+
+    /**
+     * Fill all the empty slots in the A of Length * Width;
+     * check out the implementation of "isInArea" before using this method
+     * @param length the length.
+     *               check out some geometry if you don't know what "length" is supposed to mean
+     * @param width the width
+     *              check out some geometry if you don't know what "length" is supposed to mean
+     * @param itemStack the material
+     */
+    void fillArea(int length, int width, MenuItem itemStack);
 }
