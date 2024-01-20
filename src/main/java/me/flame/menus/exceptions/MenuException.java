094c895831bbfd6c6123cbab9775874c8ad65ac3
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2024-01-20 16:40:53 +0200

    Delete src/main/java/me/flame/menus directory

--- src/main/java/me/flame/menus/exceptions/MenuException.java
+++
@@ -1,7 +0,0 @@
-package me.flame.menus.exceptions;
-
-public final class MenuException extends RuntimeException {
-    public MenuException(String message) {
-        super(message);
-    }
-}
--- src/main/java/me/flame/menus/util/ItemResponse.java
+++
@@ -1,9 +0,0 @@
-package me.flame.menus.util;
-
-import me.flame.menus.events.ClickActionEvent;
-import me.flame.menus.menu.ActionResponse;
-
-@FunctionalInterface
-public interface ItemResponse {
-    ActionResponse apply(int slot, ClickActionEvent event);
-}
