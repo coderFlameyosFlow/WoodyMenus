614f4c7db0492542dff27363d504bf6b36e666e0
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2024-01-24 11:10:30 +0200

    entire error for one letter

--- core/src/main/java/me/flame/menus/items/ItemEditor.java
+++ core/src/main/java/me/flame/menus/items/ItemEditor.java
@@ -8,7 +8,7 @@ import me.flame.menus.util.VersionHelper;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;a
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
